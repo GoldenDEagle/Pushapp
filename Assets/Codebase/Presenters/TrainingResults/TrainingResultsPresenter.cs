02aeb2521f4be2d0a77dbb681b7bd627518632da
Ivan <ionufriev95@gmail.com>
2023-10-25 19:27:02 +0600

    results presenter base

---
+++ Assets/Codebase/Presenters/TrainingResults/TrainingResultsPresenter.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Codebase.Presenters.Base;
+using Assets.Codebase.Views.Base;
+using System;
+
+namespace Assets.Codebase.Presenters.TrainingResults
+{
+    public class TrainingResultsPresenter : BasePresenter, ITrainingResultsPresenter
+    {
+        public TrainingResultsPresenter()
+        {
+            ViewId = ViewId.TrainingResultView;
+        }
+
+        public void GoNextClicked()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Codebase/Views/TrainingResults/TrainingResultsView.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Codebase.Presenter.Base;
+using Assets.Codebase.Presenters.TrainingResults;
+using Assets.Codebase.Views.Base;
+using System;
+
+namespace Assets.Codebase.Views.TrainingResults
+{
+    public class TrainingResultsView : BaseView
+    {
+        private ITrainingResultsPresenter _presenter;
+
+        public override void Init(IPresenter presenter)
+        {
+            _presenter = presenter as ITrainingResultsPresenter;
+
+            base.Init(presenter);
+        }
+
+        protected override void SubscribeToUserInput()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
